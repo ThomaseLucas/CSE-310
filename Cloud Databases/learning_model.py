70a0f23cf47b79b6834505b5887bedacde7b92ee
Thomaselucas <thomasaelucas2020@gmail.com>
2024-09-26 10:27:27 -0600

    changning it so user is able to automatically select data to add or take away from the database.

--- Cloud Databases/learning_model.py
+++ Cloud Databases/learning_model.py
@@ -12,22 +12,54 @@ def Add_Blog_Post(title, author, content):
         'author': author,
         'content': content
     })
-    print(f'Added blog post with ID: {doc_ref.id}')
+    print(f'Added blog post with ID: {doc_ref[1].id}')
+    return doc_ref[1].id
 
 def Read_Blog_Post(post_id):
     doc_ref = db.collection('posts').document(post_id)
     doc = doc_ref.get()
     if doc.exists:
-        print(f'Blog post: {doc.to_doc()}')
+        print(f'Blog Post: {doc.to_dict()}')
     else:
-        print('No such document')
+        print('No such document!')
 
-def Update_Blog_Posts(post_id, new_content){
+
+def Update_Blog_Posts(post_id, new_content):
     doc_ref = db.collection('posts').document(post_id)
     doc_ref.update({
         'content': new_content
     })
     print(f'Updated blog post with ID: {post_id}')
-}
+
+def Delete_Blog_Post(post_id):
+    doc_ref = db.collection('posts').document(post_id)
+    doc_ref.delete()
+    print(f'Deleted blog post with ID: {post_id}')
+
+def Main_Menu():
+    user_input = None
+    while user_input != 0:
+        print('1. Create blog post')
+        print('2. Read blog post')
+        print('3. Update blog post')
+        print('4. Delete blog post')
+        print('0. Exit program')
+
+        user_input = input('Please enter your selection: ')
+
+        if user_input == 1:
+            
+            blog_title = input('Enter the title of your blog post: ')
+            blog_author = input('Enter the author of your blog post: ')
+            blog_content = input('Enter the content of your blog post: ')
+            
+            doc_id = Add_Blog_Post(blog_title, blog_author, blog_content)
+
+            print(f'Your document was created with the ID: {doc_id}')
+            Read_Blog_Post(doc_id)
+        else:
+            return
+
+
 if __name__ == '__main__':
-    Add_Blog_Post('Dogs','Thomas','Dogs are cool!')
+    Main_Menu()
