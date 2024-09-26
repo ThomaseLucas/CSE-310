97ee5563ea15b35960c72e549a2e4889b887805e
Thomaselucas <thomasaelucas2020@gmail.com>
2024-09-26 09:14:47 -0600

    added update_profiles function

--- Cloud Databases/learning_model.py
+++ Cloud Databases/learning_model.py
@@ -14,5 +14,20 @@ def Add_Blog_Post(title, author, content):
     })
     print(f'Added blog post with ID: {doc_ref.id}')
 
+def Read_Blog_Post(post_id):
+    doc_ref = db.collection('posts').document(post_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        print(f'Blog post: {doc.to_doc()}')
+    else:
+        print('No such document')
+
+def Update_Blog_Posts(post_id, new_content){
+    doc_ref = db.collection('posts').document(post_id)
+    doc_ref.update({
+        'content': new_content
+    })
+    print(f'Updated blog post with ID: {post_id}')
+}
 if __name__ == '__main__':
     Add_Blog_Post('Dogs','Thomas','Dogs are cool!')
